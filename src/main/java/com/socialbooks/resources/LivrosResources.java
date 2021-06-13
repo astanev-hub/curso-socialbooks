eb32505b3e02844537a91961f247667d549fa9ff
Alexandre Stanev <astanev@hotmail.com>
2021-06-13 10:32:36 -0300

    Full CRUD implementation

--- src/main/java/com/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/socialbooks/resources/LivrosResources.java
@@ -1,25 +1,49 @@
 package com.socialbooks.resources;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.socialbooks.domain.Livro;
+import com.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping(value = "/livros")
 public class LivrosResources {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
+		return livrosRepository.findAll();
+	}
 
-		Livro l1 = new Livro("Rest Aplicado");
-		Livro l2 = new Livro("Git passo-a-passo");
-		Livro[] livros = { l1, l2};
-
-		return Arrays.asList(livros);
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Livro> buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id);
+	}	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+		livro.setId(id);
+		livrosRepository.save(livro);
+	}	
+
 }
